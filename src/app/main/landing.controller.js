ff1a1ec168f7b3c051232883dff4b630809e8445
Nicky Semenza <nicky@nickysemenza.com>
2016-04-29 16:33:49 -0400

    simple card layout test

--- src/app/main/landing.controller.js
+++ src/app/main/landing.controller.js
@@ -1,6 +1,8 @@
 angular.module('app')
 .controller('LandingController', ['$rootScope', '$scope', '$location', 'ApiRest','ngToast',
 	function($rootScope, $scope, $location, ApiRest, ngToast) {
-		
+		$scope.range = function(n) {
+        return new Array(n);
+    };
 	}
 ]);
